f5bf121afa6682aa21ded7569f270c049bcc7403
jjyoon-dev <jjyoon.dev@gmail.com>
2021-08-13 04:47:07 +0900

    no-video href 비활성화, mouse cursor default

--- portfolio/script.js
+++ portfolio/script.js
@@ -3,3 +3,6 @@ let pic = document.querySelectorAll('.pic');
 let getTitle = function() {
     return console.log((this).dataset.title);
 }
+
+// youtube 없는 링크 비활성화
+document.querySelector('.no-video').removeAttribute('href');
